5cdbc1952bd1dcea02165be296b8394d9ab94cbd
Rich Moll <drise13@gmail.com>
2022-07-14 14:31:10 -0500

    Sorted and added names for a bunch of pets

--- PetsOptimizer/Species.cs
+++ PetsOptimizer/Species.cs
@@ -8,54 +8,61 @@ using System.Runtime.Serialization;
 
 public enum Species
 {
-    [EnumMember(Value = "pincermin")] Pincermin,
-    [EnumMember(Value = "coconut")] Mafioso,
-    [EnumMember(Value = "sandcastle")] SandCastle,
-    [EnumMember(Value = "Pet3")] Mallay,
+    //W1
+    [EnumMember(Value = "mushG")] GreenMushroom,
+    [EnumMember(Value = "Pet0")] Squirrel, // A guess but likely
+    [EnumMember(Value = "frogG")] Frog,
+    [EnumMember(Value = "beanG")] BoredBean,
+    [EnumMember(Value = "mushR")] RedMushroom,
+    [EnumMember(Value = "slimeG")] Slime,
+    [EnumMember(Value = "Pet1")] Piggo, // A guess but likely
+    [EnumMember(Value = "snakeG")] BabyBoa,
+    [EnumMember(Value = "carrotO")] Carrotman,
+    [EnumMember(Value = "goblinG")] Glublin,
     [EnumMember(Value = "plank")] WodeBoard,
+    [EnumMember(Value = "frogBIG")] Gigafrog,
     [EnumMember(Value = "Pet2")] WildBoar,
-    [EnumMember(Value = "crabcake")] Crabcake,
-    [EnumMember(Value = "sheep")] Sheepie,
     [EnumMember(Value = "branch")] WalkingStick,
+    [EnumMember(Value = "acorn")] Nutto,
+    [EnumMember(Value = "poopSmall")] Poop,
+    [EnumMember(Value = "ratB")] Rat,
+
+    //W2
+    [EnumMember(Value = "jarSand")] SandyPot,
+    [EnumMember(Value = "mimicA")] Mimic,
+    [EnumMember(Value = "crabcake")] Crabcake,
+    [EnumMember(Value = "coconut")] Mafioso,
+    [EnumMember(Value = "Pet3")] Mallay,
+    [EnumMember(Value = "sandcastle")] SandCastle,
+    [EnumMember(Value = "pincermin")] Pincermin,
     [EnumMember(Value = "potato")] MashedPotato,
     [EnumMember(Value = "steak")] Tyson,
-    [EnumMember(Value = "beanG")] BoredBean,
-    [EnumMember(Value = "frogBIG")] Gigafrog,
-    [EnumMember(Value = "carrotO")] Carrotman,
-    [EnumMember(Value = "jarSand")] SandyPot,
+    [EnumMember(Value = "Pet4")] Whale, // A guess but likely
+    [EnumMember(Value = "moonman")] Moonmoon,
     [EnumMember(Value = "sandgiant")] SandGiant,
     [EnumMember(Value = "snailZ")] Snelbie,
+    [EnumMember(Value = "shovelR")] DigDoug,
+    [EnumMember(Value = "steakR")] Beefie,
+    [EnumMember(Value = "potatoB")] CrescentSpud,
+    [EnumMember(Value = "Pet5")] Chippy, // A guess but likely
+
+    //W3
+    [EnumMember(Value = "sheep")] Sheepie,
     [EnumMember(Value = "flake")] FrostFlake,
-    [EnumMember(Value = "mamoth")] Mamooth,
-    [EnumMember(Value = "poopSmall")] Poop,
-    [EnumMember(Value = "ram")] DedotatedRam,
+    [EnumMember(Value = "stache")] SirStache,
     [EnumMember(Value = "skele")] Xylobone,
-    [EnumMember(Value = "acorn")] Nutto,
+    [EnumMember(Value = "Pet6")] Bunny, // A guess but likely
     [EnumMember(Value = "bloque")] Bloque,
+    [EnumMember(Value = "mamoth")] Mamooth,
     [EnumMember(Value = "snowball")] Snowman,
+    [EnumMember(Value = "penguin")] Penguin,
     [EnumMember(Value = "thermostat")] Thermister,
     [EnumMember(Value = "glass")] Quenchie,
-    [EnumMember(Value = "penguin")] Penguin,
+    [EnumMember(Value = "snakeB")] Cryosnake,
+    [EnumMember(Value = "Pet7")] MechoMouse, // A guess but likely
     [EnumMember(Value = "speaker")] BopBox,
     [EnumMember(Value = "eye")] Neyeptune,
-    [EnumMember(Value = "mimicA")] Mimic,
-    [EnumMember(Value = "shovelR")] DigDoug,
-    [EnumMember(Value = "stache")] SirStache,
-    [EnumMember(Value = "moonman")] Moonmoon,
-    [EnumMember(Value = "slimeG")] Slime,
-    [EnumMember(Value = "steakR")] Beefie,
-    [EnumMember(Value = "goblinG")] Glublin,
-    [EnumMember(Value = "snakeG")] BabyBoa,
-    [EnumMember(Value = "snakeB")] Cryosnake,
-    [EnumMember(Value = "frogG")] Frog,
-    [EnumMember(Value = "mushG")] GreenMushroom,
-    [EnumMember(Value = "mushR")] RedMushroom,
-    [EnumMember(Value = "potatoB")] CrescentSpud,
-    [EnumMember(Value = "ratB")] MechoMouse,
-
-    [EnumMember(Value = "Pet0")] Pet0,
-    [EnumMember(Value = "Pet1")] Pet1,
-    [EnumMember(Value = "Pet4")] Pet4,
-    [EnumMember(Value = "Pet6")] Pet6,
-    [EnumMember(Value = "Pet7")] Pet7,
+    [EnumMember(Value = "ram")] DedotatedRam,
+    [EnumMember(Value = "skeleR")] Bloodbone, // A guess but likely
+    [EnumMember(Value = "Pet8")] Panda // A guess but likely
 }
