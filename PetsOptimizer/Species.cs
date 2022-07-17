297fbe21178df3b7d614cff6bfdb029913701d7d
Rich Moll <drise13@gmail.com>
2022-07-17 13:54:32 -0500

    Verified and added W4 pets

--- PetsOptimizer/Species.cs
+++ PetsOptimizer/Species.cs
@@ -10,12 +10,12 @@ public enum Species
 {
     //W1
     [EnumMember(Value = "mushG")] GreenMushroom,
-    [EnumMember(Value = "Pet0")] Squirrel, // A guess but likely
+    [EnumMember(Value = "Pet0")] Squirrel,
     [EnumMember(Value = "frogG")] Frog,
     [EnumMember(Value = "beanG")] BoredBean,
     [EnumMember(Value = "mushR")] RedMushroom,
     [EnumMember(Value = "slimeG")] Slime,
-    [EnumMember(Value = "Pet1")] Piggo, // A guess but likely
+    [EnumMember(Value = "Pet1")] Piggo,
     [EnumMember(Value = "snakeG")] BabyBoa,
     [EnumMember(Value = "carrotO")] Carrotman,
     [EnumMember(Value = "goblinG")] Glublin,
@@ -37,21 +37,21 @@ public enum Species
     [EnumMember(Value = "pincermin")] Pincermin,
     [EnumMember(Value = "potato")] MashedPotato,
     [EnumMember(Value = "steak")] Tyson,
-    [EnumMember(Value = "Pet4")] Whale, // A guess but likely
+    [EnumMember(Value = "Pet4")] Whale,
     [EnumMember(Value = "moonman")] Moonmoon,
     [EnumMember(Value = "sandgiant")] SandGiant,
     [EnumMember(Value = "snailZ")] Snelbie,
     [EnumMember(Value = "shovelR")] DigDoug,
     [EnumMember(Value = "steakR")] Beefie,
     [EnumMember(Value = "potatoB")] CrescentSpud,
-    [EnumMember(Value = "Pet5")] Chippy, // A guess but likely
+    [EnumMember(Value = "Pet5")] Chippy,
 
     //W3
     [EnumMember(Value = "sheep")] Sheepie,
     [EnumMember(Value = "flake")] FrostFlake,
     [EnumMember(Value = "stache")] SirStache,
     [EnumMember(Value = "skele")] Xylobone,
-    [EnumMember(Value = "Pet6")] Bunny, // A guess but likely
+    [EnumMember(Value = "Pet6")] Bunny,
     [EnumMember(Value = "bloque")] Bloque,
     [EnumMember(Value = "mamoth")] Mamooth,
     [EnumMember(Value = "snowball")] Snowman,
@@ -59,10 +59,28 @@ public enum Species
     [EnumMember(Value = "thermostat")] Thermister,
     [EnumMember(Value = "glass")] Quenchie,
     [EnumMember(Value = "snakeB")] Cryosnake,
-    [EnumMember(Value = "Pet7")] MechoMouse, // A guess but likely
+    [EnumMember(Value = "Pet7")] MechoMouse,
     [EnumMember(Value = "speaker")] BopBox,
     [EnumMember(Value = "eye")] Neyeptune,
     [EnumMember(Value = "ram")] DedotatedRam,
-    [EnumMember(Value = "skeleR")] Bloodbone, // A guess but likely
-    [EnumMember(Value = "Pet8")] Panda // A guess but likely
+    [EnumMember(Value = "skeleR")] Bloodbone,
+    [EnumMember(Value = "Pet8")] Panda,
+
+    //W4
+    [EnumMember(Value = "mushP")] PurpleMushroom,
+    [EnumMember(Value = "w4a2")] TV,
+    [EnumMember(Value = "w4a3")] Donut,
+    [EnumMember(Value = "demonP")] DemonGenie,
+    [EnumMember(Value = "w4b1")] FlyingWorm,
+    [EnumMember(Value = "Pet9")] Dog,
+    [EnumMember(Value = "w4b2")] SodaCan,
+    [EnumMember(Value = "w4b3")] GelatinousCubiod,
+    [EnumMember(Value = "w4b4")] Choccie,
+    [EnumMember(Value = "w4b5")] BiggoleWurm,
+    [EnumMember(Value = "Pet10")] CoolBird,
+    [EnumMember(Value = "w4c1")] Clammie,
+    [EnumMember(Value = "w4c2")] Octodar,
+    [EnumMember(Value = "w4c3")] Flombeige,
+    [EnumMember(Value = "w4c4")] StiltedSeeker,
+    [EnumMember(Value = "Pet11")] Hedgehog
 }
